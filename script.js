7fdd5fa3e69dacd26e2e7d4ee25cc6b22dfb2558
unknown <sibishanthi2000@gmail.com>
2025-12-25 19:44:27 +0530

    Added the Image

--- script.js
+++ script.js
@@ -12,3 +12,37 @@ menuToggle.addEventListener('click', () => {
     menuToggle.innerHTML = '&#8942;'; // 3 dots
   }
 });
+function fnOnClick(){
+  debugger;
+  alert("Hi");
+}
+
+const logoUpload = document.getElementById('logoUpload');
+const extraLogos = document.getElementById('extra-logos');
+logoUpload.addEventListener('change', function(event) {
+   console.log("Upload triggered!"); // check console
+    const file = event.target.files[0]; // rest of your code...
+    });
+// logoUpload.addEventListener('change', function(event) {
+//   debugger;
+//   const file = event.target.files[0];
+//   if (file && file.type === "image/jpeg") 
+//     {
+//     const reader = new FileReader();
+//     debugger;
+//     reader.onload = function(e) {
+//       // Create a new image element
+//       const newLogo = document.createElement('img');
+//       newLogo.src = e.target.result;
+//       newLogo.alt = "Uploaded Logo";
+//       newLogo.classList.add('logo-icon'); // reuse your logo styling
+
+//       // Append it to the container
+//       extraLogos.appendChild(newLogo);
+//     };
+//     reader.readAsDataURL(file);
+//   } else
+//      {
+//     alert("Please upload a PNG image.");
+//   }
+// });
